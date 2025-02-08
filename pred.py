90ab1fcf11083f3445b35013bef26914c96c60a0
Carolina <66147427+carolmfreitas@users.noreply.github.com>
2025-02-08 19:41:26 -0300

    Update pred.py

--- pred.py
+++ pred.py
@@ -295,7 +295,7 @@ def main():
         out_file = os.path.join(args.save_dir, args.model.split("/")[-1] + ".jsonl")
 
     dataset = load_dataset('THUDM/LongBench-v2', split='train') # dataset = json.load(open('data.json', 'r', encoding='utf-8'))
-    data_all = [{"_id": item["_id"], "domain": item["domain"], "sub_domain": item["sub_domain"], "difficulty": item["difficulty"], "length": item["length"], "question": item["question"], "choice_A": item["choice_A"], "choice_B": item["choice_B"], "choice_C": item["choice_C"], "choice_D": item["choice_D"], "answer": item["answer"], "context": item["context"]} for item in dataset]
+    data_all = [{"_id": item["_id"], "domain": item["domain"], "sub_domain": item["sub_domain"], "difficulty": item["difficulty"], "length": item["length"], "question": item["question"], "choice_A": item["choice_A"], "choice_B": item["choice_B"], "choice_C": item["choice_C"], "choice_D": item["choice_D"], "answer": item["answer"], "context": item["context"]} for item in dataset if item["domain"] == "Single-Document QA"]
 
     has_data = {}
     if os.path.exists(out_file):
